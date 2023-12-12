92d168da4d83439b29ef22720ab542a2c379748b
Chris <cvaccarello@brandgage.com>
2023-12-11 21:22:12 -0500

    Updated base react app with material UI for quick styling, Added a single API fetched list, Removed some clutter and refactored the base create-react-app for clarity purposes

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+const ItemList = ({ apiUrl }) => {
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ items, setItems ] = useState([]);
+
+	// this gives us a default url to use for fetching items, while optionally allowing an override if needed
+	if (!apiUrl) {
+		apiUrl = process.env.REACT_APP_API + process.env.REACT_APP_API_ITEMS_PATH;
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(apiUrl);
+				let data = await response.json();
+
+				setItems(data);
+				setIsLoading(false);
+			} catch (error) {
+				console.error(error);
+				setIsLoading(false);
+			}
+		};
+
+		fetchData().catch(console.error);
+	}, [ apiUrl ]);
+
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+
+	return (
+		<>
+			<Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+				<List>
+					{
+						items
+							// TODO: cap at 10 for now, but we can add client-side pagination later
+							.slice(0, 10)
+							.map((item) => (
+								<ListItem key={item.id}>{item.title}</ListItem>
+							))
+					}
+				</List>
+			</Box>
+		</>
+	);
+};
+
+export default ItemList;
