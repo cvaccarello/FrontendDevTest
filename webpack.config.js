1953751fdaf2df1c95fa2a978d608afcae3608a3
Chris <cvaccarello@brandgage.com>
2023-12-12 02:10:28 -0500

    Added a basic webpack process however it doesn't fully work with react's base url and needs testing with environment variables (like production vs development as well as the react environment variables I used in this project for the API URL)

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,46 @@
 
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV || true;
+
 module.exports = {
-	resolve: {
-		alias: {
-			'@mui/styled-engine': '@mui/styled-engine-sc'
-		},
+	mode: isDevelopment ? 'development' : 'production',
+	entry: {
+		'main': `./src/index.js`,
+	},
+	output: {
+		filename: '[name].min.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-env', '@babel/preset-react' ]
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: [ 'style-loader', 'css-loader' ],
+			},
+			{
+				test: /\.svg$/,
+				use: ['@svgr/webpack'],
+			},
+		]
+	},
+	plugins: [
+		new Dotenv(),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
+	optimization: {
+		minimize: !isDevelopment,
 	},
 };
