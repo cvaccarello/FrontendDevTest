1dafb2c4a463b516eba1f460da006da726666dc9
Chris <cvaccarello@brandgage.com>
2023-12-13 00:22:57 -0500

    Fixed several issues with manual webpack process  - Fixed an issue with SVGs being added with their <svg> paths, when in reality we just wanted the URL (switched to svg-url-loader),  - Fixed an issue where react wasn't working in the browser b/c it was missing a call to import react,  - Fixed an issue where the environment/mode wasn't being set correctly or using the correct env files,  - Fixed another issue where the default react index.html had templates variables for %PUBLIC_URL% which weren't able toget converted for the webpack bundled version (would like to look more into this eventually)

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,59 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const isDevelopment = process.env.NODE_ENV || true;
-console.log(process.env.NODE_ENV, isDevelopment);
 
-module.exports = {
-	mode: isDevelopment ? 'development' : 'production',
-	entry: {
-		'main': `./src/index.js`,
-	},
-	output: {
-		filename: '[name].min.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [ '@babel/preset-env', '@babel/preset-react' ]
+module.exports = (env) => {
+	const isDevelopment = env.development || true;
+	const mode = isDevelopment ? 'development' : 'production';
+
+	return {
+		mode,
+		entry: {
+			'main': `./src/index.js`,
+		},
+		output: {
+			filename: '[name].min.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+								[
+									'@babel/preset-react', {
+										// need this to enable React w/o needing to explicitly import react in the JS, I believe
+										'runtime': 'automatic'
+									}
+								]
+							]
+						}
 					}
-				}
-			},
-			{
-				test: /\.(sc|sa|c)ss$/i,
-				use: [ 'style-loader', 'css-loader', 'sass-loader' ],
-			},
-			{
-				test: /\.svg$/,
-				use: ['@svgr/webpack'],
-			},
-		]
-	},
-	plugins: [
-		new Dotenv(),
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, 'public', 'index.html'),
-		}),
-	],
-	optimization: {
-		minimize: !isDevelopment,
-	},
+				},
+				{
+					test: /\.(sc|sa|c)ss$/i,
+					use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+				},
+				{
+					test: /\.svg$/,
+					use: [ 'svg-url-loader' ],
+				},
+			]
+		},
+		plugins: [
+			new Dotenv({
+				path: `./.env.${mode}`
+			}),
+			new HtmlWebpackPlugin({
+				template: path.join(__dirname, 'public', 'index-webpack.html'),
+			}),
+		],
+		optimization: {
+			minimize: !isDevelopment,
+		},
+	}
 };
